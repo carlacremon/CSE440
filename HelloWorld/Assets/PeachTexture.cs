b77119c1ac30593fb81b8782dd1aefd51ce96dd5
Carla Cremon <carlacremon@gmail.com>
2015-10-21 15:46:22 -0700

    Hello World Assignment

---
+++ HelloWorld/Assets/PeachTexture.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeachTexture : MonoBehaviour {
+
+	private int index=0;
+	private SpriteRenderer spriteRenderer;
+	private int number;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+
+		if (Input.GetMouseButtonDown(0)){ // if left mouse button pressed...
+			GetComponent<SpriteRenderer>().color = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+			
+		}
+		
+	}
+	
+}
+

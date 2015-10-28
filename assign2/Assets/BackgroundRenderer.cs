e2d8742476e3f91f3a7ce298b89ff0898a065723
Carla Cremon <carlacremon@gmail.com>
2015-10-27 18:03:25 -0700

    assign2

---
+++ assign2/Assets/BackgroundRenderer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundRenderer : MonoBehaviour {
+	[SerializeField]
+	private float _squareSize = 10;
+	
+	public float SquareSize {
+		get { return _squareSize; }
+		set { _squareSize = value; }
+	}
+	
+	void Update () {
+		/* 	Optional: Have a check here to only call this code when
+		 *  the screen changes dimensions or when the camera is modified in any way.
+		 *	This code is called more times than it has to.
+		 */
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0));
+		transform.position = new Vector3(transform.position.x, transform.position.y, 1);
+		transform.localScale = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+		float repY = transform.localScale.y / transform.localScale.x * _squareSize;
+		
+		GetComponent<Renderer>().material.SetFloat("RepeatX", _squareSize);
+		GetComponent<Renderer>().material.SetFloat("RepeatY", repY);
+}
+}
---
+++ assign2/Assets/PlayerControll.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControll : MonoBehaviour {
+
+	public float moveSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 tempPosition = transform.position;
+		if(Input.GetAxis("Horizontal")>0){
+			tempPosition.x += moveSpeed;
+		}
+		if(Input.GetAxis("Horizontal")<0){
+			tempPosition.x -= moveSpeed;
+		}
+		if(Input.GetAxis("Vertical")<0){
+			tempPosition.y -= moveSpeed;
+		}
+		if(Input.GetAxis("Vertical")>0){
+			tempPosition.y += moveSpeed;
+		}
+
+		transform.position = tempPosition;
+
+	}
+}
